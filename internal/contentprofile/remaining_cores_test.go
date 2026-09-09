ab9cd54f2a3871586fbb3379eb60ef72dc1f9871
xxxsen <naivesen@gmail.com>
2026-09-09 23:13:16 +0800

    fix(ngpc): accept .ngc game files alongside .ngp

--- internal/contentprofile/remaining_cores_test.go
+++ internal/contentprofile/remaining_cores_test.go
@@ -6,6 +6,7 @@ func TestRemainingEmulatorJSContentAdmission(t *testing.T) {
 	t.Parallel()
 	for platform, names := range map[string][]string{
 		"zx81":       {"game.p", "game.tzx", "game.t81"},
+		"ngpc":       {"game.ngp", "game.ngc", "game.NGC"},
 		"amstradcpc": {"game.dsk", "game.sna"},
 		"pet":        {"game.prg", "game.d64"}, "plus4": {"game.prg", "game.d64"},
 		"cdi": {"game.chd"}, "pcecd": {"game.chd"},
@@ -17,6 +18,7 @@ func TestRemainingEmulatorJSContentAdmission(t *testing.T) {
 		}
 	}
 	for platform, names := range map[string][]string{
+		"ngpc": {"game.ngpc"},
 		"zx81": {"game.z80"}, "amstradcpc": {"discs.m3u", "game.kcr", "game.cpr"},
 		"pet": {"discs.m3u", "commands.cmd"}, "plus4": {"discs.vfl"},
 		"cdi": {"game.cue", "game.zip"}, "pcecd": {"game.pce", "game.cue", "game.zip"},
